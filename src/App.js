0b7e54ce07599c55362690d27df77f4adb45f88e
waqas-ahmd <65waqas@gmail.com>
2021-06-09 17:57:43 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import MainScreen from "./Components/MainScreen";
+import Menu from "./Components/Menu";
 
 function App() {
+  const [showMenu, setShowMenu] = useState(false);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <MainScreen setShowMenu={setShowMenu} />
+      <Menu setShowMenu={setShowMenu} showMenu={showMenu} />
     </div>
   );
 }
